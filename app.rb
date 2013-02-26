5186fb1b1fcbf5fe7f23a3c0ea4ce91ea507becf
Ariejan de Vroom <ariejan@ariejan.net>
2013-02-26 09:55:00 +0100

    Show real data?

--- app.rb
+++ app.rb
@@ -4,6 +4,13 @@ require 'haml'
 
 require './pivotal'
 
+DB = Sequel.connect(ENV['DATABASE_URL'])
+
 get '/' do
+  metrics = DB[:metrics]
+
+  @latest_iteration = metrics.max(:iteration)
+  @data = metrics.where(:iteration => @latest_iteration).reverse_order(:created_at)
+
   haml :index
 end
